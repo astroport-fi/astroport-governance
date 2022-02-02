786d4b744422e0fa31be0b6a30e7ca32e46e1083
Timofey Epanchintsev <epanchee@gmail.com>
2022-02-02 20:35:58 +0500

    feat(voting escrow): updated simulator

--- contracts/astro-voting-escrow/tests/simulation.rs
+++ contracts/astro-voting-escrow/tests/simulation.rs
@@ -2,18 +2,16 @@ use crate::test_utils::{mock_app, Helper};
 use anyhow::Result;
 use astroport_voting_escrow::contract::{MAX_LOCK_TIME, WEEK};
 use cosmwasm_std::{Addr, Timestamp};
-use proptest::collection::VecStrategy;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use terra_multi_test::{next_block, AppResponse, TerraApp};
 
 mod test_utils;
 
-#[derive(Clone, Default)]
+#[derive(Clone, Default, Debug)]
 struct Point {
     amount: f32,
     end: u64,
-    verified: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -22,7 +20,6 @@ enum LockEvent {
     IncreaseTime(u64),
     ExtendLock(u128),
     Withdraw,
-    NoOp,
 }
 
 use LockEvent::*;
@@ -74,127 +71,123 @@ impl Simulator {
         let periods_interval = get_period(interval);
         self.helper
             .create_lock(&mut self.router, user, interval, amount as u64)
-            .and_then(|response| {
+            .map(|response| {
                 self.add_point(
                     block_period as usize,
                     user,
                     apply_boost(amount, periods_interval),
                     block_period + periods_interval,
-                    true,
                 );
-                Ok(response)
+                response
             })
     }
 
     fn increase_time(&mut self, user: &str, interval: u64) -> Result<AppResponse> {
-        let cur_period = self.block_period() as usize;
-        let user_balance = self.calc_user_balance_at(cur_period, user);
-        let prev_point = self
-            .get_prev_point(user)
-            .expect("We always need previous point!");
         self.helper
             .extend_lock_time(&mut self.router, user, interval)
-            .and_then(|response| {
+            .map(|response| {
+                let cur_period = self.block_period() as usize;
+                let user_balance = self.calc_user_balance_at(cur_period, user);
                 self.add_point(
                     cur_period,
                     user,
                     user_balance,
-                    prev_point.end + get_period(interval),
-                    true,
+                    cur_period as u64 + get_period(interval),
                 );
-                Ok(response)
+                response
             })
     }
 
     fn extend_lock(&mut self, user: &str, amount: u128) -> Result<AppResponse> {
-        let cur_period = self.block_period() as usize;
-        let user_balance = self.calc_user_balance_at(cur_period, user);
-        let prev_point = self
-            .get_prev_point(user)
-            .expect("We always need previous point!");
-        let vp = apply_boost(amount, prev_point.end - cur_period as u64);
         self.helper
             .extend_lock_amount(&mut self.router, user, amount as u64)
-            .and_then(|response| {
-                self.add_point(cur_period, user, user_balance + vp, prev_point.end, true);
-                Ok(response)
+            .map(|response| {
+                let cur_period = self.block_period() as usize;
+                let (user_balance, end) =
+                    if let Some(point) = self.get_user_point_at(cur_period, user) {
+                        (point.amount, point.end)
+                    } else {
+                        let prev_point = self
+                            .get_prev_point(user)
+                            .expect("We always need previous point!");
+                        (self.calc_user_balance_at(cur_period, user), prev_point.end)
+                    };
+                let vp = apply_boost(amount, end - cur_period as u64);
+                self.add_point(cur_period, user, user_balance + vp, end);
+                response
             })
     }
 
     fn withdraw(&mut self, user: &str) -> Result<AppResponse> {
-        let cur_period = self.block_period();
         self.helper
             .withdraw(&mut self.router, user)
-            .and_then(|response| {
-                self.add_point(cur_period as usize, user, 0.0, cur_period, true);
-                Ok(response)
+            .map(|response| {
+                let cur_period = self.block_period();
+                self.add_point(cur_period as usize, user, 0.0, cur_period);
+                response
             })
     }
 
     fn event_router(&mut self, user: &str, event: LockEvent) {
+        println!("User {} Event {:?}", user, event);
         match event {
             LockEvent::CreateLock(amount, interval) => {
-                match self.create_lock(user, amount, interval) {
-                    Err(err) => {
-                        dbg!(err);
-                        ()
-                    }
-                    _ => (),
+                if let Err(err) = self.create_lock(user, amount, interval) {
+                    dbg!(err);
+                    ()
                 }
             }
-            LockEvent::IncreaseTime(interval) => match self.increase_time(user, interval) {
-                Err(err) => {
+            LockEvent::IncreaseTime(interval) => {
+                if let Err(err) = self.increase_time(user, interval) {
                     dbg!(err);
                     ()
                 }
-                _ => (),
-            },
-            LockEvent::ExtendLock(amount) => match self.extend_lock(user, amount) {
-                Err(err) => {
+            }
+            LockEvent::ExtendLock(amount) => {
+                if let Err(err) = self.extend_lock(user, amount) {
                     dbg!(err);
                     ()
                 }
-                _ => (),
-            },
-            LockEvent::Withdraw => match self.withdraw(user) {
-                Err(err) => {
+            }
+            LockEvent::Withdraw => {
+                if let Err(err) = self.withdraw(user) {
                     dbg!(err);
                     ()
                 }
-                _ => (),
-            },
-            LockEvent::NoOp => self.checkpoint_user(user),
+            }
         }
-        let real_balance = self.calc_user_balance_at(self.block_period() as usize, user);
+        let real_balance = self
+            .get_user_point_at(self.block_period() as usize, user)
+            .map(|point| point.amount)
+            .unwrap_or_else(|| self.calc_user_balance_at(self.block_period() as usize, user));
         let contract_balance = self
             .helper
             .query_user_vp(&mut self.router, user)
             .unwrap_or(0.0);
-        assert!((real_balance - contract_balance).abs() < 10e-5);
+        dbg!((real_balance - contract_balance).abs());
+        if !((real_balance - contract_balance).abs() < 10e-5) {
+            assert_eq!(real_balance, contract_balance)
+        };
     }
 
-    fn add_point<T: Into<String>>(
-        &mut self,
-        period: usize,
-        user: T,
-        amount: f32,
-        end: u64,
-        verified: bool,
-    ) {
+    fn checkpoint_all_users(&mut self) {
+        let cur_period = self.block_period() as usize;
+        self.users.clone().iter().for_each(|user| {
+            // we need to calc point only if it was not calculated yet
+            if self.get_user_point_at(cur_period, user).is_none() {
+                self.checkpoint_user(user)
+            }
+        })
+    }
+
+    fn add_point<T: Into<String>>(&mut self, period: usize, user: T, amount: f32, end: u64) {
         let map = &mut self.points[period];
-        map.extend(vec![(
-            user.into(),
-            Point {
-                amount,
-                end,
-                verified,
-            },
-        )]);
+        map.extend(vec![(user.into(), Point { amount, end })]);
     }
 
     fn get_prev_point(&mut self, user: &str) -> Option<Point> {
-        let cur_period = (self.block_period() - 1) as usize;
-        self.get_user_lock_at(cur_period, user)
+        let prev_period = (self.block_period() - 1) as usize;
+        self.get_user_point_at(prev_period, user)
     }
 
     fn checkpoint_user(&mut self, user: &str) {
@@ -203,10 +196,10 @@ impl Simulator {
         let prev_point = self
             .get_prev_point(user)
             .expect("We always need previous point!");
-        self.add_point(cur_period, user, user_balance, prev_point.end, true);
+        self.add_point(cur_period, user, user_balance, prev_point.end);
     }
 
-    fn get_user_lock_at<T: Into<String>>(&mut self, period: usize, user: T) -> Option<Point> {
+    fn get_user_point_at<T: Into<String>>(&mut self, period: usize, user: T) -> Option<Point> {
         let points_map = &mut self.points[period];
         match points_map.entry(user.into()) {
             Entry::Occupied(value) => Some(value.get().clone()),
@@ -215,22 +208,17 @@ impl Simulator {
     }
 
     fn calc_user_balance_at(&mut self, period: usize, user: &str) -> f32 {
-        match self.get_user_lock_at(period, user) {
-            Some(Point {
-                amount, verified, ..
-            }) if verified => amount,
-            _ => {
-                let prev_point = self.get_user_lock_at(period - 1, user).unwrap_or_default();
-                let dt = prev_point.end - period as u64 - 1;
+        match self.get_user_point_at(period, user) {
+            Some(point) => point.amount,
+            None => {
+                let prev_point = self
+                    .get_user_point_at(period - 1, user)
+                    .expect("We always need previous point!");
+                let dt = prev_point.end - (period as u64 - 1);
                 if dt == 0 {
                     0.0
                 } else {
-                    let new_vp = prev_point.amount - prev_point.amount / dt as f32;
-                    if new_vp < 0.0 {
-                        0.0
-                    } else {
-                        new_vp
-                    }
+                    prev_point.amount - prev_point.amount / dt as f32
                 }
             }
         }
@@ -238,16 +226,19 @@ impl Simulator {
 
     fn calc_total_balance_at(&mut self, period: usize) -> f32 {
         self.users.clone().iter().fold(0.0, |acc, user| {
-            acc + self.calc_user_balance_at(period, &user)
+            acc + self.calc_user_balance_at(period, user)
         })
     }
 }
 
 use proptest::prelude::*;
 
+const MAX_PERIOD: usize = 2;
+const MAX_USERS: usize = 2;
+const MAX_EVENTS: usize = 10;
+
 fn events_strategy() -> impl Strategy<Value = LockEvent> {
     prop_oneof![
-        Just(LockEvent::NoOp),
         Just(LockEvent::Withdraw),
         (0..100_u128).prop_map(LockEvent::ExtendLock),
         (0..MAX_LOCK_TIME).prop_map(LockEvent::IncreaseTime),
@@ -256,25 +247,36 @@ fn events_strategy() -> impl Strategy<Value = LockEvent> {
 }
 
 fn generate_cases() -> impl Strategy<Value = (Vec<String>, Vec<(usize, String, LockEvent)>)> {
-    let users_strategy = prop::collection::vec("[a-z]{4,32}", 3..10);
+    let users_strategy = prop::collection::vec("[a-z]{4,32}", 1..MAX_USERS);
     users_strategy.prop_flat_map(|users| {
         (
             Just(users.clone()),
             prop::collection::vec(
-                (0..105_usize, prop::sample::select(users), events_strategy()),
-                300,
+                (
+                    1..MAX_PERIOD,
+                    prop::sample::select(users),
+                    events_strategy(),
+                ),
+                0..MAX_EVENTS,
             ),
         )
     })
 }
 
 proptest! {
+    // #![proptest_config(ProptestConfig {
+    //     cases: 1,
+    //     fork: false,
+    //     .. ProptestConfig::default()
+    // })]
+
     #[test]
+    #[ignore]
     fn run_simulations
     (
         case in generate_cases()
     ) {
-        let mut events: Vec<Vec<(String, LockEvent)>> = vec![vec![]; 105];
+        let mut events: Vec<Vec<(String, LockEvent)>> = vec![vec![]; MAX_PERIOD];
         let (users, events_tuples) = case;
         for (period, user, event) in events_tuples {
             events[period].push((user, event));
@@ -284,15 +286,60 @@ proptest! {
         simulator.router.update_block(|block| block.time = Timestamp::from_seconds(0));
         for user in users {
             simulator.mint(&user, 10000);
-            simulator.event_router(&user, CreateLock(100, WEEK));
+            simulator.add_point(0, user, 0.0, 104);
         }
+        simulator.app_next_period();
 
-        for period in 0..105_usize {
+        for period in 1..MAX_PERIOD {
             if let Some(period_events) = events.get(period) {
                 for (user, event) in period_events {
-                    dbg!(period, user, event);
+                    simulator.event_router(user, event.clone())
                 }
             }
+            simulator.checkpoint_all_users();
+            simulator.app_next_period()
         }
+    }
+}
+
+#[test]
+#[ignore]
+fn exact_simulation() {
+    // TODO: does not pass yet
+    let case = (
+        ["oakjmzucckfflnhszmwilhtt"],
+        [
+            (1, "oakjmzucckfflnhszmwilhtt", CreateLock(79, 56246400)),
+            (1, "oakjmzucckfflnhszmwilhtt", ExtendLock(62)),
+            (1, "oakjmzucckfflnhszmwilhtt", ExtendLock(30)),
+            (1, "oakjmzucckfflnhszmwilhtt", ExtendLock(17)),
+            (1, "oakjmzucckfflnhszmwilhtt", ExtendLock(13)),
+            (1, "oakjmzucckfflnhszmwilhtt", ExtendLock(29)),
+        ],
+    );
+    let mut events: Vec<Vec<(String, LockEvent)>> = vec![vec![]; 105];
+    let (users, events_tuples) = case;
+    for (period, user, event) in events_tuples {
+        events[period].push((user.to_string(), event));
+    }
+
+    let mut simulator = Simulator::new(&users);
+    simulator
+        .router
+        .update_block(|block| block.time = Timestamp::from_seconds(0));
+    for user in users {
+        simulator.mint(user, 10000);
+        simulator.add_point(0, user, 0.0, 104);
+    }
+    simulator.app_next_period();
+
+    for period in 1..MAX_PERIOD {
+        if let Some(period_events) = events.get(period) {
+            for (user, event) in period_events {
+                simulator.event_router(user, event.clone())
+            }
+        }
+        simulator.checkpoint_all_users();
+        simulator.app_next_period()
     }
 }
