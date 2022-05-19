63afdf0b49211a7976cfa36f847d2df10ab6a0b4
Timofey <epanchee@gmail.com>
2022-05-19 18:24:41 +0300

    VotingEscrow contract refactoring

--- contracts/voting_escrow/src/utils.rs
+++ contracts/voting_escrow/src/utils.rs
@@ -1,7 +1,6 @@
-use crate::astroport::asset::addr_validate_to_lower;
 use crate::error::ContractError;
 use astroport_governance::utils::{get_periods_count, MAX_LOCK_TIME, WEEK};
-use cosmwasm_std::{Addr, Decimal, Deps, DepsMut, Order, Pair, StdError, StdResult, Uint128};
+use cosmwasm_std::{Addr, Decimal, Order, Pair, StdError, StdResult, Storage, Uint128};
 use cw_storage_plus::{Bound, U64Key};
 use std::cmp::min;
 use std::convert::TryInto;
@@ -17,9 +16,10 @@ pub(crate) fn time_limits_check(time: u64) -> Result<(), ContractError> {
     }
 }
 
+/// ## Description
 /// Checks that the sender is the xASTRO token.
-pub(crate) fn xastro_token_check(deps: Deps, sender: Addr) -> Result<(), ContractError> {
-    let config = CONFIG.load(deps.storage)?;
+pub(crate) fn xastro_token_check(storage: &dyn Storage, sender: Addr) -> Result<(), ContractError> {
+    let config = CONFIG.load(storage)?;
     if sender != config.deposit_token_addr {
         Err(ContractError::Unauthorized {})
     } else {
@@ -27,9 +27,10 @@ pub(crate) fn xastro_token_check(deps: Deps, sender: Addr) -> Result<(), Contrac
     }
 }
 
+/// ## Description
 /// Checks if the blacklist contains a specific address.
-pub(crate) fn blacklist_check(deps: Deps, addr: &Addr) -> Result<(), ContractError> {
-    let blacklist = BLACKLIST.load(deps.storage)?;
+pub(crate) fn blacklist_check(storage: &dyn Storage, addr: &Addr) -> Result<(), ContractError> {
+    let blacklist = BLACKLIST.load(storage)?;
     if blacklist.contains(addr) {
         Err(ContractError::AddressBlacklisted(addr.to_string()))
     } else {
@@ -37,6 +38,7 @@ pub(crate) fn blacklist_check(deps: Deps, addr: &Addr) -> Result<(), ContractErr
     }
 }
 
+/// ## Description
 /// Adjusting voting power according to the slope. The maximum loss is 103/104 * 104 which is
 /// 0.000103 vxASTRO.
 pub(crate) fn adjust_vp_and_slope(vp: &mut Uint128, dt: u64) -> StdResult<Uint128> {
@@ -45,6 +47,7 @@ pub(crate) fn adjust_vp_and_slope(vp: &mut Uint128, dt: u64) -> StdResult<Uint12
     Ok(slope)
 }
 
+/// ## Description
 /// Main function used to calculate a user's voting power at a specific period as: previous_power - slope*(x - previous_x).
 pub(crate) fn calc_voting_power(point: &Point, period: u64) -> Uint128 {
     let shift = point
@@ -57,22 +60,24 @@ pub(crate) fn calc_voting_power(point: &Point, period: u64) -> Uint128 {
         .unwrap_or_else(|_| Uint128::zero())
 }
 
+/// ## Description
 /// Coefficient calculation where 0 [`WEEK`] is equal to 1 and [`MAX_LOCK_TIME`] is 2.5.
 pub(crate) fn calc_coefficient(interval: u64) -> Decimal {
     // coefficient = 1 + 1.5 * (end - start) / MAX_LOCK_TIME
     Decimal::one() + Decimal::from_ratio(15_u64 * interval, get_periods_count(MAX_LOCK_TIME) * 10)
 }
 
+/// ## Description
 /// Fetches the last checkpoint in [`HISTORY`] for the given address.
 pub(crate) fn fetch_last_checkpoint(
-    deps: Deps,
+    storage: &dyn Storage,
     addr: &Addr,
     period_key: &U64Key,
 ) -> StdResult<Option<Pair<Point>>> {
     HISTORY
         .prefix(addr.clone())
         .range(
-            deps.storage,
+            storage,
             None,
             Some(Bound::Inclusive(period_key.wrapped.clone())),
             Order::Descending,
@@ -81,23 +86,24 @@ pub(crate) fn fetch_last_checkpoint(
         .transpose()
 }
 
-pub(crate) fn cancel_scheduled_slope(deps: DepsMut, slope: Uint128, period: u64) -> StdResult<()> {
+/// ## Description
+/// Cancels scheduled slope change of total voting power only if the given period is in future.
+/// Removes scheduled slope change if it became zero.
+pub(crate) fn cancel_scheduled_slope(
+    storage: &mut dyn Storage,
+    slope: Uint128,
+    period: u64,
+) -> StdResult<()> {
     let end_period_key = U64Key::new(period);
-    let last_slope_change = LAST_SLOPE_CHANGE
-        .may_load(deps.as_ref().storage)?
-        .unwrap_or(0);
-    match SLOPE_CHANGES.may_load(deps.as_ref().storage, end_period_key.clone())? {
+    let last_slope_change = LAST_SLOPE_CHANGE.may_load(storage)?.unwrap_or(0);
+    match SLOPE_CHANGES.may_load(storage, end_period_key.clone())? {
         // We do not need to schedule a slope change in the past
         Some(old_scheduled_change) if period > last_slope_change => {
             let new_slope = old_scheduled_change - slope;
             if !new_slope.is_zero() {
-                SLOPE_CHANGES.save(
-                    deps.storage,
-                    end_period_key,
-                    &(old_scheduled_change - slope),
-                )
+                SLOPE_CHANGES.save(storage, end_period_key, &(old_scheduled_change - slope))
             } else {
-                SLOPE_CHANGES.remove(deps.storage, end_period_key);
+                SLOPE_CHANGES.remove(storage, end_period_key);
                 Ok(())
             }
         }
@@ -105,11 +111,17 @@ pub(crate) fn cancel_scheduled_slope(deps: DepsMut, slope: Uint128, period: u64)
     }
 }
 
-pub(crate) fn schedule_slope_change(deps: DepsMut, slope: Uint128, period: u64) -> StdResult<()> {
+/// ## Description
+/// Schedules slope change of total voting power in the given period.
+pub(crate) fn schedule_slope_change(
+    storage: &mut dyn Storage,
+    slope: Uint128,
+    period: u64,
+) -> StdResult<()> {
     if !slope.is_zero() {
         SLOPE_CHANGES
             .update(
-                deps.storage,
+                storage,
                 U64Key::new(period),
                 |slope_opt| -> StdResult<Uint128> {
                     if let Some(pslope) = slope_opt {
@@ -125,6 +137,7 @@ pub(crate) fn schedule_slope_change(deps: DepsMut, slope: Uint128, period: u64) 
     }
 }
 
+/// ## Description
 /// Helper function for deserialization.
 pub(crate) fn deserialize_pair(pair: StdResult<Pair<Uint128>>) -> StdResult<(u64, Uint128)> {
     let (period_serialized, change) = pair?;
@@ -134,15 +147,16 @@ pub(crate) fn deserialize_pair(pair: StdResult<Pair<Uint128>>) -> StdResult<(u64
     Ok((u64::from_be_bytes(period_bytes), change))
 }
 
+/// ## Description
 /// Fetches all slope changes between `last_slope_change` and `period`.
 pub(crate) fn fetch_slope_changes(
-    deps: Deps,
+    storage: &dyn Storage,
     last_slope_change: u64,
     period: u64,
 ) -> StdResult<Vec<(u64, Uint128)>> {
     SLOPE_CHANGES
         .range(
-            deps.storage,
+            storage,
             Some(Bound::Exclusive(U64Key::new(last_slope_change).wrapped)),
             Some(Bound::Inclusive(U64Key::new(period).wrapped)),
             Order::Ascending,
@@ -151,15 +165,7 @@ pub(crate) fn fetch_slope_changes(
         .collect()
 }
 
-/// Bulk validation and conversion between [`String`] -> [`Addr`] for an array of addresses.
-/// If any address is invalid, the function returns [`StdError`].
-pub(crate) fn validate_addresses(deps: Deps, addresses: &[String]) -> StdResult<Vec<Addr>> {
-    addresses
-        .iter()
-        .map(|addr| addr_validate_to_lower(deps.api, addr))
-        .collect()
-}
-
+/// ## Description
 /// Calculate slashed and return amount based on a given parameters.
 /// The penalty is calculated as min(max_exit_penalty, time_left_until_unlock / MAX_LOCK_TIME).
 pub(crate) fn calc_early_withdraw_amount(
