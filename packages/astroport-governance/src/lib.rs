996ac43e7bf551e238aca10338a3886eea79afc3
Timofey <5527315+epanchee@users.noreply.github.com>
2024-04-02 18:18:48 +0400

    update astroport deps

--- packages/astroport-governance/src/lib.rs
+++ packages/astroport-governance/src/lib.rs
@@ -12,8 +12,6 @@ pub mod voting_escrow;
 pub mod voting_escrow_delegation;
 pub mod voting_escrow_lite;
 
-pub use astroport;
-
 // Default pagination constants
 pub const DEFAULT_LIMIT: u32 = 10;
 pub const MAX_LIMIT: u32 = 30;
