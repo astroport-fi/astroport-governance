e8a716c9c61630aa7b758c42b907689fac068224
Timofey <5527315+epanchee@users.noreply.github.com>
2025-05-15 15:36:33 -0300

    fix(tributes): store cw2 info (#122)

--- contracts/tributes/src/instantiate.rs
+++ contracts/tributes/src/instantiate.rs
@@ -1,15 +1,21 @@
 use astroport::asset::validate_native_denom;
-use astroport_governance::emissions_controller::utils::get_epoch_start;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{ensure, DepsMut, Env, MessageInfo, Response};
+use cw2::set_contract_version;
 
+use astroport_governance::emissions_controller::utils::get_epoch_start;
 use astroport_governance::tributes::{
     Config, InstantiateMsg, REWARDS_AMOUNT_LIMITS, TOKEN_TRANSFER_GAS_LIMIT,
 };
 
 use crate::error::ContractError;
 use crate::state::CONFIG;
+
+/// Contract name that is used for migration.
+pub const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
+/// Contract version that is used for migration.
+pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 /// Creates a new contract with the specified parameters in the [`InstantiateMsg`].
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -38,6 +44,8 @@ pub fn instantiate(
         TOKEN_TRANSFER_GAS_LIMIT.contains(&msg.token_transfer_gas_limit),
         ContractError::InvalidTokenTransferGasLimit {}
     );
+
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
     CONFIG.save(
         deps.storage,
